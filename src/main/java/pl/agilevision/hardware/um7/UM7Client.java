e9b428da81f9cd555ff2dc04499bedaf05a66e79
Volodymyr Rudyi <volodymyr@agilevision.pl>
2016-11-07 19:22:14 +0200

    UM7 Client interface

---
+++ src/main/java/pl/agilevision/hardware/um7/UM7Client.java
@@ -0,0 +1,7 @@
+package pl.agilevision.hardware.um7;
+
+/**
+ * Created by volodymyr on 07.11.16.
+ */
+public interface UM7Client {
+}
---
+++ src/test/java/pl/agilevision/hardware/um7/MockTest.java
@@ -0,0 +1,14 @@
+package pl.agilevision.hardware.um7;
+
+import org.junit.Test;
+
+/**
+ * Created by volodymyr on 07.11.16.
+ */
+public class MockTest {
+
+  @Test
+  public void testMock(){
+
+  }
+}
