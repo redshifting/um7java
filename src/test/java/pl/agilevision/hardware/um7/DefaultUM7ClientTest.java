bee0d8369debda179960768e946afb82f43968dd
Volodymyr Rudyi <volodymyr@agilevision.pl>
2016-11-09 23:42:07 +0200

    Add more tests

--- src/test/java/pl/agilevision/hardware/um7/DefaultUM7ClientTest.java
+++ src/test/java/pl/agilevision/hardware/um7/DefaultUM7ClientTest.java
@@ -1,8 +1,15 @@
 package pl.agilevision.hardware.um7;
 
 import org.junit.Test;
+import pl.agilevision.hardware.um7.data.UM7Packet;
 import pl.agilevision.hardware.um7.exceptions.DeviceConnectionException;
+import pl.agilevision.hardware.um7.exceptions.OperationTimeoutException;
 import pl.agilevision.hardware.um7.impl.DefaultUM7Client;
+import pl.agilevision.hardware.um7.impl.UM7Constants;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -12,14 +19,15 @@ import static org.junit.Assert.assertTrue;
  */
 public class DefaultUM7ClientTest extends AbstractDeviceTest{
 
-  private static final java.lang.String TEST_PORT_NAME = "COM3";
+  private static final String TEST_PORT_NAME = "COM3";
+  private static final String TEST_DEVICE_NAME = "um7";
 
   @Test
   public void testConnect() throws DeviceConnectionException {
-    info("Connect/disconnect check");
+    info("Connect check");
 
     // Given
-    final UM7Client client = new DefaultUM7Client("um7", TEST_PORT_NAME);
+    final UM7Client client = new DefaultUM7Client(TEST_DEVICE_NAME, TEST_PORT_NAME);
 
     // Then
     assertTrue(client.isConnected());
@@ -32,7 +40,7 @@ public class DefaultUM7ClientTest extends AbstractDeviceTest{
     info("Connect/disconnect check");
 
     // Given
-    final UM7Client client = new DefaultUM7Client("um7", TEST_PORT_NAME);
+    final UM7Client client = new DefaultUM7Client(TEST_DEVICE_NAME, TEST_PORT_NAME);
 
     // Then
     assertTrue(client.isConnected());
@@ -43,5 +51,45 @@ public class DefaultUM7ClientTest extends AbstractDeviceTest{
     assertTrue(client.isConnected());
 
     client.disconnect();
+  }
+
+  @Test
+  public void testReadRegistry() throws DeviceConnectionException, OperationTimeoutException {
+    info("Read registry check");
+    final UM7Client client = new DefaultUM7Client(TEST_DEVICE_NAME, TEST_PORT_NAME);
+
+    try{
+
+      // Given
+      final Map<String, Integer> registers = new HashMap<>();
+      registers.put("DREG_HEALTH", UM7Constants.Registers.DREG_HEALTH);
+      registers.put("DREG_GYRO_RAW_XY", UM7Constants.Registers.DREG_GYRO_RAW_XY);
+      registers.put("DREG_GYRO_PROC_X", UM7Constants.Registers.DREG_GYRO_PROC_X);
+      registers.put("DREG_ACCEL_PROC_X", UM7Constants.Registers.DREG_ACCEL_PROC_X);
+      registers.put("DREG_EULER_PHI_THETA", UM7Constants.Registers.DREG_EULER_PHI_THETA);
+      registers.put("DREG_GYRO_BIAS_X", UM7Constants.Registers.DREG_GYRO_BIAS_X);
+      registers.put("CREG_COM_SETTINGS", UM7Constants.Registers.CREG_COM_SETTINGS);
+      registers.put("CREG_GYRO_TRIM_X", UM7Constants.Registers.CREG_GYRO_TRIM_X);
+      registers.put("CREG_MAG_CAL1_1", UM7Constants.Registers.CREG_MAG_CAL1_1);
+      registers.put("CREG_MAG_BIAS_X", UM7Constants.Registers.CREG_MAG_BIAS_X);
+
+      registers.put("H_CREG_GYRO_ALIGN1_1", UM7Constants.Registers.H_CREG_GYRO_ALIGN1_1);
+      registers.put("H_CREG_ACCEL_ALIGN1_1", UM7Constants.Registers.H_CREG_ACCEL_ALIGN1_1);
+      registers.put("H_CREG_MAG_ALIGN1_1", UM7Constants.Registers.H_CREG_MAG_ALIGN1_1);
+      registers.put("H_CREG_MAG_REF", UM7Constants.Registers.H_CREG_MAG_REF);
+
+      // Then
+      final Base64.Encoder encoder = Base64.getEncoder();
+
+      for(final Map.Entry<String, Integer> entry : registers.entrySet()){
+        
+        final UM7Packet dataPacket = client.readRegistry(entry.getValue());
+        final String message = String.format("Register [%-15s] contains value [%s]", entry.getKey(), encoder.encodeToString(dataPacket.data));
+        System.out.println(message);
+      }
+
+    } finally {
+      client.disconnect();
+    }
   }
 }
